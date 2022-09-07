643ce42e7b519f461cbb55bd010b70eb34559de2
abhishekgarfield <abhishek2759@gmail.com>
2022-09-07 18:02:01 +0530

    filter foreach

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ catch(err)
 {
     console.log(err);
 }
+
+var arr=[1,2,3];
+var arr1=[...arr,6];
+console.log(arr1);
+
+var arr2=arr1.map((item)=>{
+return item>3;
+})
+console.log(arr2);
+
+var ar=[{
+    user_id:"2",
+    name:"abhi"
+},{
+    user_id:"3",
+    name:"sharma"
+}]
+
+const ne=ar.map(({user_id})=>user_id)
+console.log(ne);
