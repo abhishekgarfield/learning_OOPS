d9b8c13425a0b3a585512ec7687a4165f0ab5dc6
abhishekgarfield <abhishek2759@gmail.com>
2022-09-10 14:09:24 +0530

    examples

--- dynamicfrop.js
+++ dynamicfrop.js
@@ -1,2 +1,23 @@
-const cityUrl = "https://zomatoajulypi.herokuapp.com/location"
-const restUrl = "https://zomatoajulypi.herokuapp.com/restaurant?stateId="
+const cityUrl = "https://zomatoajulypi.herokuapp.com/location";
+const restUrl = "https://zomatoajulypi.herokuapp.com/restaurant?stateId=";
+
+
+function loadCity() {
+  console.log("here");
+  fetch(cityUrl, { method: "GET" })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+     var  city = data;
+      printcity();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+const printcity=()=>{
+    var parent= document.getElementById("city");
+        var child=parent.lastChild;
+        city.map((item))}
---
+++ functions.js
@@ -0,0 +1,8 @@
+const add = (display) => {
+  var c = a + b;
+  display(c);
+};
+
+const display = (c) => {
+  console.log(c);
+};
