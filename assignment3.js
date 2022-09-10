25ddba8163323ef13bd13c11606888f13b377961
abhishekgarfield <abhishek2759@gmail.com>
2022-09-10 23:24:30 +0530

    assignment 3 done

--- assignment3.js
+++ assignment3.js
@@ -1,11 +1,91 @@
-class restaurantList {
-    constructor(name,address,city)
+/* --------------- Assignment-3 -------------*/
+/* --------------- Question 1 --------------*/
+
+class restaurantManager {
+  restaurantList = [
     {
-        this.name=name;
-        this.address=address;
-        this.city=city;
-    }
-    printAllRestaurantNames(){
-            console.log(this.name);
-   }
+      id: 1,
+      restaurantName: "KFC",
+      address: "Anand Vihar",
+      city: "Delhi",
+    },
+    {
+      id: 2,
+      restaurantName: "Domino",
+      address: "Savita Vihar",
+      city: "Delhi",
+    },
+    {
+      id: 3,
+      restaurantName: "Burger King",
+      address: "Civil Lines",
+      city: "Pune",
+    },
+    {
+      id: 4,
+      restaurantName: "Subway",
+      address: "Cantonment",
+      city: "Mumbai",
+    },
+  ];
+  printAllRestaurantNames() {
+    this.restaurantList.map(({ restaurantName }) => {
+      console.log(restaurantName);
+    });
+  }
+  filterRestaurantByCity(cit) {
+    this.restaurantList.filter((item) => {
+      if (item.city == cit) {
+        console.log(item);
+      }
+    });
+  }
+}
+
+const obj = new restaurantManager();
+obj.printAllRestaurantNames();
+obj.filterRestaurantByCity("Pune");
+
+/* ---------------- Question 2 -----------------*/
+
+orderData = {
+  "Below 500": 20,
+  "500-1000": 29,
+  "1000-1500": 30,
+  "1500-2000": 44,
+  "Above 2000": 76,
+};
+
+// a) Total number of orders
+
+var sum = 0;
+Object.values(orderData).forEach((item) => {
+  sum += item;
+});
+
+/* ------------- Another way ------------ */
+
+for (keys in orderData) {
+  sum += orderData[keys];
+}
+console.log(sum);
+
+// b) Calculate the number of order proportion options
+
+var orderPerportions = Object.keys(orderData).length;
+console.log(orderPerportions);
+
+// c) Calculate the percentage of each proportion
+
+var arr = [];
+
+for (i = 0; i < Object.keys(orderData).length; i++) {
+  var objec = {};
+  objec.id = i;
+  objec.order = Object.keys(orderData)[i];
+  objec.restaurant = "Punjabi tadka";
+  objec.order_percentage = ((Object.values(orderData)[i] / sum) * 100).toFixed(
+    2
+  );
+  arr.push(objec);
 }
