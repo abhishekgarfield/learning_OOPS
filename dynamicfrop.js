5da2cccec494363041605e739dd2b43672d7a43a
abhishekgarfield <abhishek2759@gmail.com>
2022-09-10 16:47:28 +0530

    integrated city and restaurant

--- dynamicfrop.js
+++ dynamicfrop.js
@@ -23,23 +23,40 @@ const printcity = (city) => {
   var child = parent.lastChild;
   city.map(({ location_name }) => {
     console.log(location_name);
-    textNode = document.createTextNode(`${location_name}`);
+    var textNode = document.createTextNode(`${location_name}`);
     elementNode = document.createElement("option");
     elementNode.appendChild(textNode);
     parent.appendChild(elementNode);
   });
-  parent.addEventListener("change",(e)=>{
+  parent.addEventListener("change", (e) => {
     console.log(e.currentTarget.value);
-    var stateId=0;
+    var stateId = 0;
+
+    var el2 = document.getElementById("rest");
+    while(el2.children.length>1)
+    {
+        el2.removeChild(el2.children[1]);
+        
+    }
     city.forEach((element) => {
-    if(element.location_name==e.currentTarget.value ){stateId=element.state_id}       
+      if (element.location_name == e.currentTarget.value) {
+        stateId = element.state_id;
+      }
     });
     const restUrl = `https://zomatoajulypi.herokuapp.com/restaurant?stateId=${stateId}`;
-    fetch(restUrl,{method:"GET"})
-    .then((res)=>{return res.json()})
-    .then((data)=>{console.log("rest");console.log(data); data.map(({restaurant_name})=>{})});
-
-
-  })
+    fetch(restUrl, { method: "GET" })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("rest");
+        console.log(data);
+        data.map(({ restaurant_name }) => {
+          var textNode = document.createTextNode(`${restaurant_name}`);
+          var el3 = document.createElement("option");
+          el3.appendChild(textNode);
+          el2.appendChild(el3);
+        });
+      });
+  });
 };
-
