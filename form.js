af88fba0c5201f0009c218e55685795ab339db46
abhishekgarfield <abhishek2759@gmail.com>
2022-09-11 22:44:58 +0530

    regex

--- form.js
+++ form.js
@@ -13,12 +13,12 @@ function validateFname(){
 const validateEmail = () => {
     let email = document.getElementById('email').value;
     if(email.trim().length == 0){
-        document.getElementById('eout').innerText="Please Enter Email Id"
+        eout.innerText="Please Enter Email Id"
     }else{
-        if(email.match("^([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})$") == null){
-            document.getElementById('eout').innerText="Please Enter Valid Email"
+        if(email.match("^([a-zA-Z0-9]+)@([a-zA-Z0-9]+).([a-zA-Z]{2,5})$") == null){
+            eout.innerText="Please Enter Valid Email"
         }else{
-            document.getElementById('eout').innerText=""
+            eout.innerText=""
         }
     }
 }
